622768a7131fb1ad79523aa9dabb754f35fc3ec4
CassianoE <kssycs@outlook.com>
2024-05-09 17:59:31 -0300

    populando as listas

--- src/app/components/editora/editoralist/editoralist.component.ts
+++ src/app/components/editora/editoralist/editoralist.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../menu/menu.component';
+import { ListaComponent } from '../../lista/lista.component';
 
 @Component({
   selector: 'app-editoralist',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, ListaComponent],
   templateUrl: './editoralist.component.html',
   styleUrl: './editoralist.component.scss',
 })
-export class EditoralistComponent {}
+export class EditoralistComponent {
+  editoras = [
+    { id: 1, nome: 'Editora A' },
+    { id: 2, nome: 'Editora B' },
+    { id: 3, nome: 'Editora C' },
+    // Adicione mais editoras conforme necessário
+  ];
+}
--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EditoralistComponent } from '../editora/editoralist/editoralist.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [],
+  imports: [EditoralistComponent, NgFor],
   templateUrl: './lista.component.html',
-  styleUrl: './lista.component.scss'
+  styleUrl: './lista.component.scss',
 })
 export class ListaComponent {
+  @Input() editoras: any[]; // Defina a propriedade de entrada editoras
 
+  constructor() {
+    this.editoras = []; // Inicialize a propriedade editoras no construtor
+  }
 }
