1fe0c622966c5ff24bfba25655394d6f72b30b54
CassianoE <kssycs@outlook.com>
2024-05-10 12:32:32 -0300

    arrumando conflitos

---
+++ src/app/components/lista/lista.component.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  Input,
+  TemplateRef,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { EditoralistComponent } from '../editora/editoralist/editoralist.component';
+import { CommonModule, NgFor } from '@angular/common';
+import {
+  MdbModalModule,
+  MdbModalRef,
+  MdbModalService,
+} from 'mdb-angular-ui-kit/modal';
+
+@Component({
+  selector: 'app-lista',
+  standalone: true,
+  imports: [EditoralistComponent, NgFor, CommonModule, MdbModalModule],
+  templateUrl: './lista.component.html',
+  styleUrl: './lista.component.scss',
+})
+export class ListaComponent {
+  @Input() data: any[]; // Defina a propriedade de entrada editoras
+  @Input('nomeDaLista') nomeDaLista: string = '';
+  constructor() {
+    this.data = []; // Inicialize a propriedade editoras no construtor
+  }
+
+  modalService = inject(MdbModalService); // conseguir abrir a modal pelo TS
+  @ViewChild('modal') modalDetalhe!: TemplateRef<any>; //enxergar o bloco de html da modal
+  modalRef!: MdbModalRef<any>; //conseguir fechar a modal aberta pelo TS
+
+  abrirModalEditar() {
+    alert('Modal aberto');
+  }
+
+  adicionarNovoItem() {
+    const novoItem = window.prompt('Nome da editora a ser adicionada');
+    if (novoItem) {
+      this.data.push({ id: this.data.length + 1, nome: novoItem });
+    }
+  }
+}
