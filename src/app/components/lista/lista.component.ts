afb2e721ec430d0e7f4205fbfb82d525287923ed
CassianoE <kssycs@outlook.com>
2024-05-13 21:27:40 -0300

    implementando os botoes novo e editar

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -14,38 +14,69 @@ import {
 } from 'mdb-angular-ui-kit/modal';
 import { Data } from '@angular/router';
 import { Livro } from '../../models/livro/livro';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-lista',
   standalone: true,
-  imports: [NgFor, CommonModule, MdbModalModule],
+  imports: [NgFor, CommonModule, MdbModalModule, FormsModule, MdbFormsModule],
   templateUrl: './lista.component.html',
   styleUrl: './lista.component.scss',
 })
 export class ListaComponent {
-  @Input() data: any[]; // Defina a propriedade de entrada editoras
+  @Input() itens: any[]; // Defina a propriedade de entrada editoras
   @Input('nomeDaLista') nomeDaLista: string = '';
+  idItemSalvar!: number;
+  campoAtualizado: string = '';
+  indiceAtualizando!: number;
+  campoNovo: string = '';
   constructor() {
-    this.data = [];
+    this.itens = [];
   }
 
   modalService = inject(MdbModalService); // conseguir abrir a modal pelo TS
   @ViewChild('modal') modalDetalhe!: TemplateRef<any>; //enxergar o bloco de html da modal
+  @ViewChild('modalnovo') modalnovo!: TemplateRef<any>;
   modalRef!: MdbModalRef<any>; //conseguir fechar a modal aberta pelo TS
 
-  deleteById(data: { id: number }) {
-    let indice = this.data.findIndex((x) => {
-      return x.id === data.id;
+  deleteById(id: number ) {
+    let indice = this.itens.findIndex((x) => {
+      return x.id === id;
     });
-    this.data.splice(indice, 1);
+    this.itens.splice(indice, 1);
   }
 
-  abrirModalEditar() {
+  abrirModalEditar(id: number ) {
+    //abre modal passando o id do item a ser editado
+    this.idItemSalvar = id;
     this.modalRef = this.modalService.open(this.modalDetalhe);
   }
 
-  AtualizarItem() {
-    // Fechar a modal
+  abrirModalNovo() {
+    this.modalRef = this.modalService.open(this.modalnovo);
+  }
+  salvarItem() {
+  
+    this.itens.push({ id: this.itens[this.itens.length - 1].id + 1, nome: this.campoNovo });
+    this.modalRef.close();
+    this.campoNovo = '';
+  }
+
+  novoItem() {
+    this.itens.push({ id: this.idItemSalvar, nome: this.campoAtualizado });
+    this.modalRef.close();
+    this.idItemSalvar = 0;
+  }
+  AtualizarItem(id: number ) {
+    let indice = this.itens.findIndex(x => x.id === id)
+
+    this.indiceAtualizando = indice;
+
+    alert(this.campoAtualizado)
+    if (this.itens[indice]) {
+      this.itens[indice].nome = this.campoAtualizado;
+    }
     this.modalRef.close();
   }
 }
